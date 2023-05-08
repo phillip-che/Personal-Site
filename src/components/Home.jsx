3d3576dc1f5e8bd39b03567f52d2116dbb5ba674
phillip-che <pche@cpp.edu>
2023-05-08 02:17:27 -0700

    implement spotify api call to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,28 @@
 import "../styles/Home.css";
 import { Fade } from "react-awesome-reveal";
+import { useEffect } from "react";
+import axios from "axios";
+const CLIENT_ID = import.meta.env.VITE_APP_CLIENT_ID;
+const CLIENT_SECRET = import.meta.env.VITE_APP_CLIENT_SECRET;
+
 const Home = () => {
+
+    useEffect(() => {
+        const callAPI = async () => {
+            await axios("https://accounts.spotify.com/api/token", {
+                headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+                },
+                data: "grant_type=client_credentials",
+                method: "POST",
+            }).then((token) => {
+                console.log(token);
+            })
+        }
+        callAPI();
+    })
+    
     return (
             <div>
             <Fade triggerOnce duration={2000}>
@@ -11,7 +33,7 @@ const Home = () => {
             <Fade direction="right">
                 <p>you're average boba enjoyer trying to navigate through the struggles of being an aspiring software engineer.</p>
                 <p>other interests include real estate, personal finance, and reselling</p>
-                {/* display spotify statistics: 5 most recently played tracks, top 5 tracks */}
+                {/* what i'm listening to: display spotify 5 most recently played tracks */}
                 {/* perhaps a photo gallery with carousel or deck of cards animation */}
             </Fade>
             </div>
