cc8dc54e18517c6860994a2b45160ce96d8b326a
phillip-che <pche@cpp.edu>
2023-05-16 00:11:05 -0700

    Update gallery and home

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,19 @@
 import "../styles/Gallery.css";
 import GalleryCards from "./GalleryCards";
+import { Fade } from "react-awesome-reveal";
 
 const Gallery = () => {
   return (
-    <div>
-        <GalleryCards />
+    <div id="gallery">
+        <Fade triggerOnce duration={2000}>
+            <div className="section-header">
+                <h1>/gallery</h1>
+            </div>
+        </Fade>
+        <Fade direction="right">
+            <p>here are some random moments captured along my journey . . .</p>
+            <GalleryCards />
+        </Fade>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -89,8 +89,7 @@ const Home = () => {
                 </Fade>
                 <Fade direction="right" triggerOnce>
                     <p>you're average boba enjoyer trying to navigate through the struggles of being an aspiring software engineer.</p>
-                    {/* <p>other interests include real estate, personal finance, and reselling</p> */}
-                    <p>here are some random moments captured along my journey . . .</p>
+                    <p>other interests include real estate, personal finance, and reselling</p>
                     {/* what i'm listening to: display spotify 5 most recently played tracks */}
                 
                     {/* {nowPlaying ? (
