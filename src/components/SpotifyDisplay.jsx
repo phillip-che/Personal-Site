8ca4096b4deb6499e1cc432d8fd5139a300f6313
phillip-che <pche@cpp.edu>
2023-05-17 18:38:59 -0700

    Add spotify display for top tracks

--- src/components/SpotifyDisplay.jsx
+++ src/components/SpotifyDisplay.jsx
@@ -2,20 +2,30 @@ import { Spotify } from "react-spotify-embed"
 
 const SpotifyDisplay = ({nowPlaying, recentlyPlayed, topTracks}) => {            
     return (
-        <div>
+        <div id="spotify">
             Currently listening to . . .
             <Spotify 
-            className="now-playing-track" 
+            className="now-playing-track"
             // wide 
             link={`https://open.spotify.com/track/${nowPlaying.id}`} />
-            <div>
+            
+            <div className="recently-played-container">
                 {recentlyPlayed && recentlyPlayed.map((track) => (
                     <Spotify 
-                    // className="now-playing-track" 
+                    className="track" 
                     wide 
                     link={`https://open.spotify.com/track/${track.songID}`} />
                 ))}
             </div>
+            <div className="top-tracks-container">
+                {topTracks && topTracks.map((track) => (
+                    <Spotify 
+                    className="track" 
+                    wide 
+                    link={`https://open.spotify.com/track/${track.songID}`} />
+                ))}
+            </div>
+            
         </div>
     ) 
 }
